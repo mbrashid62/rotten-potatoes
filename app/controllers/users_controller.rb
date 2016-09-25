b90120d2186664be6d8e91b673acb1b6104cc962
Bill Rashid <mohammad-rashid@uiowa.edu>
2016-09-24 22:31:04 -0500

    finished part 2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,10 +16,17 @@ class UsersController < ApplicationController
     # simply renders view
   end
 
+  # noinspection RailsChecklist01
   def create
-    @user = User.create_user!(user_params) # todo make sure user id is unique - where to do this?
-    flash[:notice] = "Welcome #{@user.email}, your account has been successfully created."
-    redirect_to root_path # if successful registration redirect to home page
+   if User.user_id_exists? user_params[:user_id]
+     flash[:notice] = "Sorry, that user id is taken already. Please try again"
+     redirect_to users_path
+   else
+     @user = User.create_user!(user_params)
+     flash[:notice] = "Welcome #{@user.email}, your account has been successfully created."
+     redirect_to root_path # if successful registration redirect to home page
+   end
+
   end
 
   def edit
@@ -31,6 +38,6 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    
+
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,12 @@
 class User < ActiveRecord::Base
-  def create_user! (user_params)
+
+  def self.create_user! (user_params)
     session_token = SecureRandom.base64
-    user_params.merge(session_token: session_token)
-    self.create_user user_params
+    user_params[:session_token] = session_token
+    create!(user_params)
+  end
+
+  def self.user_id_exists? (user_id)
+    User.where(:user_id => user_id).exists?
   end
 end
